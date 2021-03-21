5019370f4d758eaaf38b9a79394b300400cc9c69
obinasBaba <54037700+obinasBaba@users.noreply.github.com>
2021-03-21 14:44:32 +0300

    nav is responsive for mouse click

--- js/nav.js
+++ js/nav.js
@@ -1,2 +1,29 @@
-let a = 5;
-console.log(a);
+const navDots = document.body.querySelector('.pagination__nav');
+
+navDots.addEventListener('click', evt => {
+    if (!evt.target.classList.contains('dot'))
+        return;
+
+    if (evt.target.classList.contains('active'))
+        return;
+
+    const activeDot = navDots.querySelector('.dot.active');
+    let clickedDot = evt.target;
+
+    Array.from(activeDot.children)
+        .forEach((child, index) => {
+            if (child.classList.contains('page__no'))
+                child.textContent = `0${Array.from(navDots.children).indexOf(clickedDot) + 1}`;
+
+            if(child.classList.contains('page__name'))
+                child.lastElementChild.textContent = clickedDot.dataset.name;
+
+            activeDot.removeChild(child);
+            clickedDot.appendChild(child);
+        })
+
+    clickedDot.classList.add('active');
+    activeDot.classList.remove('active');
+
+
+})
---
+++ js/sliderNav.js
@@ -0,0 +1,17 @@
+
+
+const hamburger = document.body.querySelector('.nav__bar .humberger');
+const nav_slider = document.body.querySelector('.slider__nav__wrapper');
+
+    // Full Screen Navigation
+(() => {
+    hamburger.addEventListener('click', e => {
+        nav_slider.classList.add('show');
+    })
+
+    nav_slider.addEventListener('click', e => {
+        if (e.target === nav_slider || e.target.classList.contains('close-btn')) {
+            nav_slider.classList.remove('show');
+        }
+    })
+})()

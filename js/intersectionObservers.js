9e1de162fb0288a21e4c132664b037c5d3ac5f60
obinasBaba <54037700+obinasBaba@users.noreply.github.com>
2021-04-09 09:06:36 +0300

    intersection working

--- js/intersectionObservers.js
+++ js/intersectionObservers.js
@@ -1,71 +1,34 @@
-const paginationNav = document.body.querySelector('.pagination__nav');
+import {insert} from "./util.js";
 
+const paginationNav = document.body.querySelector('.pagination__nav');
 let options = {
     root: null,
     rootMargin: '0px 0px 0px 0px',
-    threshold: [.6] // number of times the callback execute
+    threshold: [.55] // number of times the callback execute
 };
-let callback = (e, ib) => {
 
-    e.forEach(ee => {
+let callback = (el, ib) => {
 
-        if (ee.isIntersecting) {
+    if (el[0].isIntersecting) {
+        let active = Array.from(paginationNav.children)
+                          .find(e => e.classList.contains('active'))
+        let clicked = Array.from(paginationNav.children)
+            .find(e => e.dataset.name === el[0].target.id)
 
-            let t = ee.target.id;
-            // console.log(ee.target.className, ' - true ')
+        if (active === clicked)
+            return;
 
-            switch (t) {
-                case 'welcome': {
-                    console.log('welcome');
-                    let ac = paginationNav.querySelector('.active')
-
-                    ac.classList.remove('active-black');
-
-                    console.log('class list', ac.classList)
-                }
-                    break;
-                case 'about': {
-                    console.log('about');
-                    let ac = paginationNav.querySelector('.active');
-                    ac.classList.remove('active-black');
-
-                    console.log('class list', ac.classList)
-
-                }
-                    break;
-                case 'service': {
-                    console.log('service');
-
-                }
-                    break;
-                case 'portfolio': {
-                    console.log('portfolio');
-
-                }
-                    break;
-                case 'blog': {
-                    console.log('blog');
-
-                }
-                    break;
-                case 'contact': {
-                    console.log('contact');
-
-                }
-                    break;
-
-                default:
-                    console.log('default', t)
-            }
+        insert(active, clicked)
+    }
 
 
-            console.log(paginationNav)
 
-        }
-
-    })
 }
-let observer = new IntersectionObserver(callback, options);
+
+const Observer = new IntersectionObserver(callback, options);
+
+
 let targets = document.body.querySelectorAll('section');
 
-targets.forEach(target => observer.observe(target));
+
+targets.forEach(target => Observer.observe(target));
--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,5 @@
+import {insert} from "./util.js";
+
 const navDots = document.body.querySelector('.pagination__nav');
 const track = navDots.querySelector('.name__track');
 
@@ -12,10 +14,8 @@ trackChildren.forEach((name, index) => {
     name.style.top = (trackHeight * index) + `px`;
 })
 
-//function to slide(transform) them on click
-const slideTo = (track, current, target) => {
-    track.style.transform = "translateY(-" + (target.style.top) + ")";
-};
+
+
 
 const dotsClickEvent = evt => {
     if (!evt.target.classList.contains('dot')) return;
@@ -24,68 +24,10 @@ const dotsClickEvent = evt => {
 
     const activeDot = navDots.querySelector('.dot.active');
     const clickedDot = evt.target;
-    const clickedIndex = Array.from(navDots.children).indexOf(clickedDot) ;
 
-    //the transition will start here
-    activeDot.style.opacity = '0';
-    clickedDot.style.opacity = '0'
-
-    //function to run after the above(opacity) transition end;
-    const insertElements = () => {
-        //remove and add from the previous active to newly active child
-        Array.from(activeDot.children)
-            .forEach((child, index) => {
-                if (child.classList.contains('page__no'))
-                    child.textContent = `0${clickedIndex + 1}`;
-
-                activeDot.removeChild(child);
-                clickedDot.appendChild(child);
-            })
-
-        clickedDot.classList.add('active');
-        activeDot.classList.remove('active');
-
-        // scrolling to the clicked section
-        //dynamically adding a 'anchor' and removing it after...
-        scrollToClicked(clickedDot);
-
-        //return to the initial visibility
-        activeDot.style.opacity = '1';
-        clickedDot.style.opacity = '1';
-
-        //we have to remove the callback for the next time so that it keep the cycle
-        activeDot.removeEventListener('transitionend', insertElements);
-    }
-
-    //add the callback after the transition end;
-    activeDot.addEventListener('transitionend', insertElements )
-
-
-    //run the slide animation after everything run (async)
-    // for the transition effect to tack place
-    setTimeout(() => {
-        const activeTrackName =
-            trackChildren.find(elems => elems.classList.contains('active_name'))
-        slideTo(track, activeTrackName, trackChildren[clickedIndex]);
-    }, 0)
+    insert(activeDot, clickedDot, true)
 
 }
-
-
-function scrollToClicked(clickedDot) {
-    new Promise(((resolve, reject) => {
-        let link = document.createElement('a',);
-        link.href = `#${clickedDot.dataset.name}`
-        console.log(link)
-        document.body.append(link);
-        link.click();
-        resolve(link);
-    })).then(v => {
-        document.body.removeChild(v);
-    })
-}
-
-
 
 navDots.addEventListener('click', dotsClickEvent)
 
